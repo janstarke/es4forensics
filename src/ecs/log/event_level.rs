5d3e90e0336d4dddce819fe323a1244f0376815c
Jan Starke <jan.starke@t-systems.com>
2022-11-14 16:19:14 +0100

    add conversion vom Value to EventLevel

--- src/ecs/log/event_level.rs
+++ src/ecs/log/event_level.rs
@@ -1,5 +1,8 @@
+use anyhow::bail;
 use num_derive::{FromPrimitive, ToPrimitive};
+use num_traits::FromPrimitive;
 use serde::Serialize;
+use serde_json::Value;
 
 /// Source: <https://learn.microsoft.com/de-de/dotnet/api/system.diagnostics.tracing.eventlevel?view=net-6.0>
 #[derive(strum_macros::Display, FromPrimitive, ToPrimitive, Serialize)]
@@ -11,4 +14,18 @@ pub enum EventLevel {
     Warning = 3,
     Information = 4,
     Verbose = 5,
+}
+
+impl TryFrom<&Value> for EventLevel {
+    type Error = anyhow::Error;
+
+    fn try_from(value: &Value) -> Result<Self, anyhow::Error> {
+        match value.as_u64() {
+            None => bail!("unable to convert '{value}' into u8"),
+            Some(n) => match Self::from_u64(n) {
+                None => bail!("invalid numeric value: '{n}'"),
+                Some(me) => Ok(me)
+            }
+        }
+    }
 }
